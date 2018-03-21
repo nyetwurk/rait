ec51307a38a36870c46f2364d048e0513823e5b2
U-VAIO\nyet <nyet@nyet.org>
2018-03-20 22:17:38 -0700

    Fix issue #1 - use existing tabs if present to preserve back button history

--- background.js
+++ background.js
@@ -7,21 +7,29 @@ browser.contextMenus.create({
 var oldTabs;
 var leaves = 0;
 
-function createAndRemoveOldTabs(children) {
-    createTabs(children);
-    // only remove if we opened at least one new tab, or the window will close
-    if (leaves>0)
+function updateAndRemoveOldTabs(children) {
+    updateTabList(children);
+    // remove leftover tabs, but only if we made new tabs, otherwise
+    // there will be nothing left and the window will close.
+    if (leaves>0 && oldTabs.length>0) {
 	browser.tabs.remove(oldTabs);
+    }
 }
 
-function createTabs(children) {
+function updateTabList(children) {
     for (child of children) {
 	if (child.url != null) {
-	    browser.tabs.create({ url:child.url });
+	    if (oldTabs.length>0) {
+		// update existing tab
+	        browser.tabs.update(oldTabs.shift(), { url:child.url })
+	    } else {
+		// make new tab
+		browser.tabs.create({ url:child.url });
+	    }
 	    leaves++;
 	} else {
 	    // recurse
-	    browser.bookmarks.getChildren(child.id).then(createTabs);
+	    browser.bookmarks.getChildren(child.id).then(updateTabList);
 	}
     }
 }
@@ -30,7 +38,7 @@ function replaceAllInTabs(id) {
     browser.tabs.query({ currentWindow:true }).then(
 	function(tabs) {
 	    oldTabs = tabs.map(function(a) { return a.id; });
-	    browser.bookmarks.getChildren(id).then(createAndRemoveOldTabs);
+	    browser.bookmarks.getChildren(id).then(updateAndRemoveOldTabs);
 	}
     );
 }
