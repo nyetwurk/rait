8d4abcd485e411ca361abc869974593ed1906ede
U-VAIO\nyet <nyet@nyet.org>
2018-03-24 14:07:26 -0700

    Minor refactoring to increase readibility

--- background.js
+++ background.js
@@ -9,9 +9,8 @@ browser.contextMenus.create({
 });
 
 var storedOptions;
-var oldTabs;
-var tasks;
 var recurseTasks;
+var updateTasks;
 
 function showError(title, error)
 {
@@ -19,15 +18,18 @@ function showError(title, error)
     browser.tabs.executeScript({code: window});
 }
 
-function updateAndRemoveOldTabs(children) {
-    tasks = [];
+function updateAndRemoveOldTabs(oldTabs, bookmarks) {
     recurseTasks = [];
-    updateBookmarks(children);
-    /* traverse recursed tab list to fill flat oldTabs list */
-    Promise.all(recurseTasks).then(function() {
-	/* traverse tabs to be opened */
-	Promise.all(tasks).then(arrayOfResults => {
-	    // get list of successes
+    updateTasks = [];
+    /* gather recurseTasks and updateTasks */
+    recurseBookmarks(oldTabs, bookmarks);
+    /* traverse bookmark list */
+    Promise.all(recurseTasks)
+    .then(function() {
+	/* traverse existing tabs, possibly to be updated */
+	Promise.all(updateTasks)
+        .then(arrayOfResults => {
+	    /* get list of successes */
 	    let ok = arrayOfResults.filter(v => v.ok);
 	    // remove leftover tabs, but only if we have new tabs, otherwise
 	    // there will be nothing left and the window will close.
@@ -35,7 +37,7 @@ function updateAndRemoveOldTabs(children) {
 		ok.length>0 && oldTabs.length>0) {
 		browser.tabs.remove(oldTabs);
 	    }
-	    // get list of failures
+	    /* get list of failures */
 	    let errs = arrayOfResults.map(v => v.err).filter(Boolean);
 	    if (errs.length>0)
 		showError("Could not load bookmark(s):", errs.join('\\n'));
@@ -46,7 +48,7 @@ function updateAndRemoveOldTabs(children) {
 /* Give a promise return values and add it to the task list */
 function loadTab(promise)
 {
-    tasks.push(
+    updateTasks.push(
 	promise.then(
 	    tab => {return {ok:true}},
 	    e => {return {ok:false, err:e.message}}
@@ -55,35 +57,44 @@ function loadTab(promise)
 }
 
 /* recursively generate list of old tabs and bookmark loading promises */
-function updateBookmarks(children) {
-    for (child of children) {
+function recurseBookmarks(oldTabs, bookmarks) {
+    for (child of bookmarks) {
 	if (child.url != null) {
 	    let url = { url:child.url };
 	    if (oldTabs.length>0) {
-		// update existing tab
+		/* update existing tab */
 		loadTab(browser.tabs.update(oldTabs.shift(), url))
 	    } else {
-		// make new tab
+		/* make new tab */
 		loadTab(browser.tabs.create(url));
 	    }
 	} else {
-	    // recurse if enabled.
-	    if (storedOptions.indexOf("recurse") != -1)
-		recurseTasks.push(browser.bookmarks.getChildren(child.id)
-		    .then(updateBookmarks));
+	    /* recurse if enabled */
+	    if (storedOptions.indexOf("recurse") != -1) {
+		let getting =
+		    browser.bookmarks.getChildren(child.id)
+		    .then(bookmarks => recurseBookmarks(oldTabs, bookmarks))
+		recurseTasks.push(getting);
+	    }
 	}
     }
 }
 
 function replaceAllInTabs(id) {
-    browser.storage.local.get().then(options => {
+    browser.storage.local.get()
+    .then(options => {
+	/* copy options */
 	storedOptions = options.options;
-	browser.tabs.query({ currentWindow:true }).then(
-	    function(tabs) {
-		oldTabs = tabs.map(function(a) { return a.id; });
-		browser.bookmarks.getChildren(id).then(updateAndRemoveOldTabs);
-	    }
-	);
+	/* grab bookmarks of this bookmark id */
+	bookmarkPromise = browser.bookmarks.getChildren(id);
+	/* grab current open tabs */
+	browser.tabs.query({ currentWindow:true })
+	.then(tabs => {
+	    /* extract array of tab ids */
+	    let tabIds = tabs.map(a => a.id);
+	    /* traverse bookmarks */
+	    bookmarkPromise.then(bms => updateAndRemoveOldTabs(tabIds, bms))
+	});
     });
 }
 
